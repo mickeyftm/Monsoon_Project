53a6cc21cbf8a1142a22d272289176555ab8de03
Davyd Tevzadze <pawtod22@gmail.com>
2021-06-23 08:52:34 +0300

    second part of scripts

--- app/js/app.js
+++ app/js/app.js
@@ -12,43 +12,78 @@ window.$ = $
 
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, MorphSVGPlugin)
 
-// require('jquery.marquee');
-
-$('.marquee').marquee({
-	duration: 14000,
-	gap: 0,
-	duplicated: true,
-	startVisible: true,
-
-})
-
-var circle = document.getElementById('mainDiv');
-
 document.addEventListener('DOMContentLoaded', () => {
+	gsap.to(window, { duration: 0.1, scrollTo: 0, ease: 'power2', delay: 0 });
 
-	let arrow = document.querySelector('.footer-menu-center');
-	// Custom JS
-	//buttons
-	let cursor = new Cursor(document.querySelector('.cursor'));
-	let button = new ButtonCtrl(document.querySelector('.binance'));
-	let buttonTwo = new ButtonCtrl(document.querySelector('.testnet'));
+	//preolader
+	let body = document.querySelector('body')
+	let preloader = document.querySelector('.preloader')
+	let preloaderIcon = document.querySelector('.preloader_icon')
+	let logo = document.querySelector('.logo')
+	let counter = document.querySelector('.counter')
+	let seconds = document.querySelector('.seconds')
+	function count() {
+		let number = 100
+		let secondNumber = 100
+		setInterval(() => {
+			
+			if(number > 0) {
+				number = number-1
+				counter.innerHTML = `${number}`
+				secondNumber = 100
+				
+				// secondNumber = secondNumber-1
+				// seconds.innerHTML = `${secondNumber}`
+				setInterval(() => {
+					if(secondNumber > 0) {
+						secondNumber = secondNumber-1
+						seconds.innerHTML = `${secondNumber}`
+					} else {
+						return
+					}
+					
+				}, 3);
+			} else {
+				return
+			}
+		}, 25);
+	}
+	count()
+	setTimeout(() => {
+		preloader.classList.add('loaded')
+		body.classList.remove('loading')
+	}, 3000);
 
-	buttonTwo.on('enter', () => {
-		cursor.enter()
-	});
-	buttonTwo.on('leave', () => {
-		cursor.leave()
-	});
+	gsap.to(preloaderIcon, {
+		autoAlpha: 0,
+		duration: 1,
+		delay: 4,
+	})
+	let counterWrapper = document.querySelector('.counter-wrapper')
 
-	button.on('enter', () => {
-		cursor.enter()
-	});
-	button.on('leave', () => {
-		cursor.leave()
-	});
+	gsap.to(counterWrapper, {
+		autoAlpha: 0,
+		duration: 0.3,
+		delay: 3,
+	})
 
-	//FAQ
+	gsap.to(logo, {
+		autoAlpha: 1,
+		duration: 1,
+		delay: 4,
+	})
 
+	//marquee 
+
+	$('.marquee').marquee({
+		duration: 14000,
+		gap: 0,
+		duplicated: true,
+		startVisible: true,
+	})
+
+
+	//mobile_menu
 	let mobileButton = document.querySelector('.bt-menu')
 	let mobileMenu = document.querySelector('.mobile_menu')
 
@@ -57,7 +92,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		mobileButton.classList.toggle('active')
 	})
 
-
+	//FAQ
 	let accordionItemHeaders = document.querySelectorAll('.accordion-item-header');
 	let accordions = document.querySelectorAll('.stars_open')
 	let accordionsText = document.querySelectorAll('.accordion-item-body')
@@ -83,42 +118,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	}
 
-	//product screen scrool function
-	// let product = document.querySelector('.products')
-	// let productRight = document.querySelector('.products-right')
-	// let header = document.querySelector('header')
-	// let requireOffset = header.getBoundingClientRect().height
-	// let productTop
-	// let productBottom
-	// function productSroll() {
-	// 	productTop = product.getBoundingClientRect().y
-	// 	productBottom = product.getBoundingClientRect().y + product.getBoundingClientRect().height - window.innerHeight
-	// 	// console.log(productBottom)
-	// 	if(productTop < requireOffset && !productRight.classList.contains('sticky') && !productRight.classList.contains('bottom')) {
-	// 		productRight.classList.add('sticky')
-	// 		console.log('1')
-	// 	} else if(productTop > requireOffset && productRight.classList.contains('sticky')) {
-	// 		productRight.classList.remove('sticky')
-	// 		console.log('2')
-	// 	}
-
-	// 	if(productBottom < -requireOffset && productRight.classList.contains('sticky')) {
-	// 		productRight.classList.add('bottom')
-	// 		productRight.classList.remove('sticky')
-	// 		console.log(productRight.getBoundingClientRect().y + productRight.getBoundingClientRect().height - window.innerHeight)
-	// 		console.log('3')
-	// 	} else if (productBottom > -requireOffset && productRight.classList.contains('bottom') && !productRight.classList.contains('sticky')) {
-	// 		productRight.classList.add('sticky')
-	// 		productRight.classList.remove('bottom')
-	// 		console.log('4')
-	// 	}
-
-	// }
-
-	// window.addEventListener('scroll', () => {
-	// 	productSroll()
-	// })
-
+	//products animation
 	ScrollTrigger.create({
 		trigger: '.products',
 		start: "top top",
@@ -127,25 +127,30 @@ document.addEventListener('DOMContentLoaded', () => {
 	})
 
 	//header hide
-	function headerAnimation() {
+	var header = document.querySelector('header');
+	let headerHeight = header.getBoundingClientRect().height
 
+	window.addEventListener('scroll', () => {
+		if (window.scrollY > headerHeight && !header.classList.contains('fixed')) {
+			header.classList.add('fixed')
+			setTimeout(() => {
+				header.style.transition = 'top 0.3s ease'
+				console.log('asdalskdj')
+			}, 400);
+
+		} else if (window.scrollY == 0 && header.classList.contains('fixed')) {
+			header.classList.remove('fixed')
+			header.style.removeProperty('transition')
+		}
+	})
+	function headerAnimation() {
 		var doc = document.documentElement;
 		var w = window;
-
 		var prevScroll = w.scrollY || doc.scrollTop;
 		var curScroll;
 		var direction = 0;
 		var prevDirection = 0;
-
-		var header = document.querySelector('header');
-
 		var checkScroll = function () {
-
-			/*
-			** Find the direction of scroll
-			** 0 - initial, 1 - up, 2 - down
-			*/
-
 			curScroll = w.scrollY || doc.scrollTop;
 			if (curScroll > prevScroll) {
 				//scrolled up
@@ -159,15 +164,10 @@ document.addEventListener('DOMContentLoaded', () => {
 			if (direction !== prevDirection) {
 				toggleHeader(direction, curScroll);
 			}
-
 			prevScroll = curScroll;
 		};
-
 		var toggleHeader = function (direction, curScroll) {
 			if (direction === 2 && curScroll > 52) {
-
-				//replace 52 with the height of your header in px
-
 				header.classList.add('hide');
 				prevDirection = direction;
 			}
@@ -182,71 +182,142 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	headerAnimation()
 
-	//inwiev animation
-	let animItems = document.querySelectorAll('.anim-items')
-	function inviewAnimation() {
-		for (let i = 0; i < animItems.length; i++) {
-			ScrollTrigger.create({
-				trigger: animItems[i],
-				start: 'top 100%',
-				end: 'bottom 0%',
-				// markers: true,
-				toggleActions: 'restart reverse restart reverse',
-				// invalidateOnRefresh: true,
-				onEnter: () => {
-					gsap.fromTo(animItems[i], 0.7,
-						{ autoAlpha: 0, y: '0.4rem' },
-						{ autoAlpha: 1, y: 0, ease: 'power2', delay: 0.3 });
-				},
-				onEnterBack: () => {
-					gsap.fromTo(animItems[i], 0.7,
-						{ autoAlpha: 0, y: '-0.4rem' },
-						{ autoAlpha: 1, y: 0, ease: 'power2', delay: 0.3 });
-				},
-				onLeave: () => {
-					gsap.fromTo(animItems[i], 0.7,
-						{ autoAlpha: 1, y: 0 },
-						{ autoAlpha: 0, y: '0.4rem', ease: 'power2' });
-				},
-				onLeaveBack: () => {
-					gsap.fromTo(animItems[i], 0.7,
-						{ autoAlpha: 1, y: 0 },
-						{ autoAlpha: 0, y: '-0.4rem', ease: 'power2' });
-				}
-			})
-
-		}
-	}
-
-	// inviewAnimation()
-
 	//to top on footer
 	let footerHandle = document.querySelector('.footer-menu-center')
-	let body = document.querySelector('body')
 
 	footerHandle.addEventListener('click', () => {
 		gsap.to(window, { duration: 1, scrollTo: 0, ease: 'power2' });
 	})
-    //mackground SVG animation
-	
-	function backgroundSvgAnimation() {
-		gsap.to('#start', {duration: 1, morphSVG: '#end'})
-	}	
-	let sections = document.querySelectorAll('section')
-	let sectionsTopOffset = []
-	let isInwievd = []
-	for (let i = 0; i < sections.length; i++) {
-		sectionsTopOffset.push(sections[i].getBoundingClientRect().y)
+
+	//footer animation  on inwiev
+	let footer = document.querySelector('footer')
+	let faq = document.querySelector('.faq')
+	let faqOffsetBottom = faq.getBoundingClientRect().y + faq.getBoundingClientRect().height - window.innerHeight
+
+	function footerAnimation() {
+		window.addEventListener('scroll', () => {
+			faqOffsetBottom = faq.getBoundingClientRect().y + faq.getBoundingClientRect().height - window.innerHeight
+			if (faqOffsetBottom < 20) {
+				footer.classList.add('active')
+			} else if (faqOffsetBottom > 20 && footer.classList.contains('active')) {
+				footer.classList.remove('active')
+			}
+		})
 	}
+
+	footerAnimation()
+
+	//buttons & cursor
+	let cursor = new Cursor(document.querySelector('.cursor'));
+	let button = new ButtonCtrl(document.querySelector('.binance'));
+	let buttonTwo = new ButtonCtrl(document.querySelector('.testnet'));
+
+	let cursorHoverItems = document.querySelectorAll('.cursor-hover')
+	for (let i = 0; i < cursorHoverItems.length; i++) {
+		cursorHoverItems[i].addEventListener('mouseover', () => {
+			cursor.enter()
+		});
+		cursorHoverItems[i].addEventListener('mouseleave', () => {
+			cursor.leave()
+		});
+	}
+
+	//inwiev + svg animations
 	function inviewCore() {
-		for (let i = 0; i < sections.length; i++) {
-			sectionsTopOffset[i] = sections[i].getBoundingClientRect().y
-		}
-		console.log(sectionsTopOffset)
+		heroInwiev()
+		processInwiev()
+		ensuringInwiev()
 	}
-	inviewCore()
-	window.addEventListener('scroll', () => {
+
+	let backgroundSvg = document.querySelector('.back')
+
+	function heroInwiev() {
+		let enablingSection = document.querySelector('.enabling')
+		let heroLines = document.querySelectorAll('.enabling .line')
+		let animItems = enablingSection.querySelectorAll('.anim-items')
+		let headerItems = document.querySelectorAll('header .anim-items')
+		for (let i = 0; i < heroLines.length; i++) {
+			heroLines[i].classList.add('visible')
+		}
+		header.classList.add('visible')
+		gsap.to(backgroundSvg, { autoAlpha: 1, duration: 1 })
+		gsap.to(animItems, { y: 0, autoAlpha: 1, stagger: 0.07 });
+		gsap.to(headerItems, { y: 0, autoAlpha: 1, stagger: 0.07 });
+
+	}
+
+	function processInwiev() {
+		let animItems = document.querySelectorAll('.process .anim-items')
+
+		ScrollTrigger.create({
+			trigger: '.process',
+			start: 'top center',
+			// end: 'bottom ',
+			// markers: true,
+			onEnter: () => {
+				gsap.to('#one-hero', { duration: 1, x: 0, y: 0, fill: "#8349FF", morphSVG: '#one-process', ease: 'power1' })
+				gsap.to('#two-hero', { duration: 1, x: '5rem', y: '1rem', fill: "#FF0095", morphSVG: '#two-process', ease: 'power1' })
+				gsap.to('#three-hero', { duration: 1, x: 0, y: '2.5rem', fill: "#0013FF", morphSVG: '#three-process', ease: 'power1' })
+				gsap.to('#four-hero', { duration: 1, x: 0, y: '2.5rem', fill: "#00FFFF", morphSVG: '#four-process', ease: 'power1' })
+				gsap.to(animItems, { y: 0, autoAlpha: 1, stagger: 0.07 });
+				backgroundSvg.classList.add('process')
+			},
+			onLeave: () => {
+				backgroundSvg.classList.remove('process')
+			},
+			onEnterBack: () => {
+				gsap.to('#one-hero', { duration: 1, x: 0, y: 0, fill: "#8349FF", morphSVG: '#one-process', ease: 'power1' })
+				gsap.to('#two-hero', { duration: 1, x: '5rem', y: '1rem', fill: "#FF0095", morphSVG: '#two-process', ease: 'power1' })
+				gsap.to('#three-hero', { duration: 1, x: 0, y: '2.5rem', fill: "#0013FF", morphSVG: '#three-process', ease: 'power1' })
+				gsap.to('#four-hero', { duration: 1, x: 0, y: '2.5rem', fill: "#00FFFF", morphSVG: '#four-process', ease: 'power1' })
+				backgroundSvg.classList.add('process')
+			},
+			onLeaveBack: () => {
+				gsap.to('#one-hero', { duration: 1, x: '-.3rem', y: '-.5rem', fill: "#280075", morphSVG: '#one-hero', ease: 'power1' })
+				gsap.to('#two-hero', { duration: 1, x: 0, y: '.2rem', fill: "#CF00A3", morphSVG: '#two-hero', ease: 'power1' })
+				gsap.to('#three-hero', { duration: 1, x: 0, y: '1.5rem', fill: "#FF8993", morphSVG: '#three-hero', ease: 'power1' })
+				gsap.to('#four-hero', { duration: 1, x: 0, y: 0, fill: "#FF0095", morphSVG: '#four-hero', ease: 'power1' })
+				backgroundSvg.classList.remove('process')
+			}
+		})
+	}
+
+	function ensuringInwiev() {
+		let animItems = document.querySelectorAll('.ensuring .anim-items')
+		ScrollTrigger.create({
+			trigger: '.ensuring',
+			start: 'top center',
+			// end: 'bottom ',
+			// markers: true,
+			onEnter: () => {
+				// gsap.to('#one-hero', { duration: 1, x: 0, y: 0, fill: "#8349FF", morphSVG: '#one-process', ease: 'power1' })
+				// gsap.to('#two-hero', { duration: 1, x: '5rem', y: '1rem', fill: "#FF0095", morphSVG: '#two-process', ease: 'power1' })
+				// gsap.to('#three-hero', { duration: 1, x: 0, y: '2.5rem', fill: "#0013FF", morphSVG: '#three-process', ease: 'power1' })
+				// gsap.to('#four-hero', { duration: 1, x: 0, y: '2.5rem', fill: "#00FFFF", morphSVG: '#four-process', ease: 'power1' })
+				gsap.to(animItems, { y: 0, autoAlpha: 1, stagger: 0.07 });
+				backgroundSvg.classList.add('ensuring')
+			},
+			onLeave: () => {
+				backgroundSvg.classList.remove('ensuring')
+			},
+			onEnterBack: () => {
+				// gsap.to('#one-hero', { duration: 1, x: 0, y: 0, fill: "#8349FF", morphSVG: '#one-process', ease: 'power1' })
+				// gsap.to('#two-hero', { duration: 1, x: '5rem', y: '1rem', fill: "#FF0095", morphSVG: '#two-process', ease: 'power1' })
+				// gsap.to('#three-hero', { duration: 1, x: 0, y: '2.5rem', fill: "#0013FF", morphSVG: '#three-process', ease: 'power1' })
+				// gsap.to('#four-hero', { duration: 1, x: 0, y: '2.5rem', fill: "#00FFFF", morphSVG: '#four-process', ease: 'power1' })
+				backgroundSvg.classList.add('ensuring')
+			},
+			onLeaveBack: () => {
+				backgroundSvg.classList.remove('ensuring')
+			}
+		})
+	}
+
+
+	setTimeout(() => {
 		inviewCore()
-		console.log(body.getBoundingClientRect().y)
-	})
+	}, 4000);
+
+	//window on top, after page is loaded
+	gsap.to(window, { duration: 0.1, scrollTo: 0, ease: 'power2', delay: 1 });
 })
